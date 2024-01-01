63a1c532c08852ec8f3ce2ccf85b5e09b9a10d2a
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-01-01 18:18:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import routes from './src/routes/routes.js';
+import * as database from './src/utils/database.js';
+import handleUnknownEndpoint from './src/utils/middleware.js';
+
+database.connectToDatabase();
 
 const app = express();
 
+// Use API routes
 app.use(routes);
+
+// Handle unknown API endpoints
+app.use(handleUnknownEndpoint);
 
 export default app;
